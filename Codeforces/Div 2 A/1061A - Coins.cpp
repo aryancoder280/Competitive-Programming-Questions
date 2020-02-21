ebe3909f20c994e19ca137acd5b10fe352e7429c
aryancoder280 <41503577+aryancoder280@users.noreply.github.com>
2020-02-21 18:10:54 +0530

    1061A - Coins.cpp

---
+++ Codeforces/Div 2 A/1061A - Coins.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define M 1000000007
+using namespace std;
+typedef long long int ll;
+
+
+int main()
+{
+      int a,b;
+      cin>>a>>b;
+      int ans = ceil(double(b)/a);
+      cout<< ans;
+
+}
